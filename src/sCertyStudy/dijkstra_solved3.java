9aa55273964af0c8b13f558b45a7c7126a537c1c
iPadOwner <zenith21c@naver.com>
2022-06-29 08:21:03 +0900

    feature6 add

--- src/sCertyStudy/dijkstra_solved3.java
+++ src/sCertyStudy/dijkstra_solved3.java
@@ -10,7 +10,7 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 
-//this is from feature 5555555
+//this is from feature 6
 public class dijkstra_solved3 {
 	static class Node implements Comparable<Node>{
 		public int end;
